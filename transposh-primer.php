49ad47e0cff62fcb1ad6506656a387a361a454ff
Heiko Mamerow <mail@heikomamerow.de>
2017-04-25 12:53:47 +0200

    renaming

--- transposh-primer.php
+++ transposh-primer.php
@@ -1,28 +1,23 @@
 <?php
 
-/*
-Plugin Name: Tp Primer
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A brief description of the Plugin.
-Version: 1.0
-Author: Heiko Mamerow
-Author URI: http://URI_Of_The_Plugin_Author
-License: A "Slug" license name e.g. GPL2
+/**
+ * Plugin Name:		Transposh Primer
+ * Plugin URI:		https://github.com/HeikoMamerow/transposh-primer
+ * Description:		Priming pages with transposh translation.
+ * Version:			1.1.0
+ * Author:			Heiko Mamerow
+ * Author URI:		https://heikomamerow.de
+ * License:			GPL-2.0+
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:		transposh-primer
+ * Domain Path:		/languages
+ */
 
-Usage:
-- Write a widget
-*/
-
-//include_once plugin_dir_path( __FILE__ ) . 'transposh-translation-filter-for-wordpress/transposh.php';
 
 // Add meta box for page edit.
 function tp_primer_add_custom_box() {
-	add_meta_box(
-		'tp_primer_box_id',
-		'Transposh Primer',
-		'tp_primer_custom_box_html',  // Content callback, must be of type callable
-		'page'
-	);
+	add_meta_box( 'tp_primer_box_id', 'Transposh Primer', 'tp_primer_custom_box_html',  // Content callback, must be of type callable
+		'page' );
 }
 
 add_action( 'add_meta_boxes', 'tp_primer_add_custom_box' );
@@ -66,8 +61,8 @@ function tp_primer_enqueue() {
 	$screen = get_current_screen();
 	if ( is_object( $screen ) ) {
 		if ( in_array( $screen->post_type, [ 'page' ] ) ) {
-			wp_enqueue_style( 'tp-primer-css', plugins_url( 'css/tp-primer.css', __FILE__ ) );
-			wp_enqueue_script( 'tp-primer-js', plugins_url( 'js/tp-primer.js', __FILE__ ), array(), '1.0.0', true );
+			wp_enqueue_style( 'tp-primer-css', plugins_url( 'css/transposh-primer.css', __FILE__ ) );
+			wp_enqueue_script( 'tp-primer-js', plugins_url( 'js/transposh-primer.js', __FILE__ ), array(), '1.0.0', true );
 		}
 	}
 }
